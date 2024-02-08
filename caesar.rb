13e444d00a80224672303a6fb088a415ab5b3ec8
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-02-08 11:33:36 +1100

    Added main algorithm to encrypt the text.     - able to wrap if result key goes beyond z/Z. Added user prompt for text and key.

--- caesar.rb
+++ caesar.rb
@@ -1,20 +1,26 @@
-# puts "Enter a text: "
-# txtOriginal = gets.chomp
+# User input for text and key
+puts "Enter a text: "
+txtOriginal = gets.chomp
+puts "Enter a KEY (between 0 - 26): "
+key = gets.chomp
 
+# Encrypt function.
 def caesar_cipher(txt, key)
   txtCiphered = ""
 
   txt.each_char do |ch|
     ascii = ch.ord
-    if ascii >= 65 && ascii <= 90
-      txtCiphered += (ascii - key).chr
-    elsif ascii >= 97 && ascii <= 122
-      txtCiphered += (ascii - key).chr
+    if (ascii >= 65 && ascii <= 90)
+      txtCiphered += ((ascii + key - 65) % 26 + 65).chr
+    elsif (ascii >= 97 && ascii <= 122)
+      txtCiphered += ((ascii + key - 97) % 26 + 97).chr
     else
-      txtCiphered += (ascii - key).chr
+      txtCiphered += ch
     end
   end
 
-  return txtCiphered
+  return puts txtCiphered
 
 end
+
+caesar_cipher(txtOriginal, key.to_i)
